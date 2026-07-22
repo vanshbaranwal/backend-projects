9e016428732516a38ce73f4930294d31b36a8cf0
Vansh Baranwal <vanshveerbaranwal@gmail.com>
2026-07-22 23:50:29 +0530

    implemented sign-up route

--- youtube-api/index.js
+++ youtube-api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import fileUpload from "express-fileupload";
+import bodyParser from "body-parser";
 
 import { ConnectDB } from "./config/db.config.js";
 import userRoutes from "./routes/user.routes.js";
@@ -9,6 +10,13 @@ dotenv.config();
 
 const app = express();
 ConnectDB();
+
+app.use(bodyParser.json());
+
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+}));
 
 app.use("/api/v1/user", userRoutes);
 
--- youtube-api/routes/user.routes.js
+++ youtube-api/routes/user.routes.js
@@ -2,12 +2,34 @@ import express from "express";
 import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 import User from "../models/user.model.js";
+import cloudinary from "../config/cloudinary.js";
 
 const router = express.Router();
 
 router.post("/signup", async(req, res) => {
     try {
-        const hashcode = await bcrypt.hash(req.body.password, 10);
+        const hashedpassword = await bcrypt.hash(req.body.password, 10);
+        const uploadImage = await cloudinary.uploader.upload(
+            req.files.logo.tempFilePath
+        );
+        console.log("image : ", uploadImage);
+
+        const newUser = new User({
+            _id: new mongoose.Schema.Types.ObjectId,
+            email: req.body.email,
+            password: hashedpassword,
+            channelName: req.body.channelName,
+            phone: req.body.phone,
+            logoUrl: uploadImage.secure_url,
+            logoId: uploadImage.public_id
+        });
+
+        let user = await newUser.save();
+
+        res.status(201).json({
+            user
+        })
+
     } catch (error) {
         
     }
