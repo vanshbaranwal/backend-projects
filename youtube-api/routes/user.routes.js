e04dc5dbd90b1c9ee854532311220b786873ea7e
Vansh Baranwal <vanshveerbaranwal@gmail.com>
2026-07-23 13:49:05 +0530

    implemented login route

--- youtube-api/routes/user.routes.js
+++ youtube-api/routes/user.routes.js
@@ -3,19 +3,22 @@ import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 import User from "../models/user.model.js";
 import cloudinary from "../config/cloudinary.js";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
 router.post("/signup", async(req, res) => {
     try {
+        console.log("request coming");
         const hashedpassword = await bcrypt.hash(req.body.password, 10);
+        console.log(hashedpassword);
         const uploadImage = await cloudinary.uploader.upload(
-            req.files.logo.tempFilePath
+            req.files.logoUrl.tempFilePath
         );
         console.log("image : ", uploadImage);
 
         const newUser = new User({
-            _id: new mongoose.Schema.Types.ObjectId,
+            _id: new mongoose.Types.ObjectId,
             email: req.body.email,
             password: hashedpassword,
             channelName: req.body.channelName,
@@ -28,12 +31,72 @@ router.post("/signup", async(req, res) => {
 
         res.status(201).json({
             user
-        })
+        });
 
     } catch (error) {
-        
+        console.log(error);
+        res.status(500).json({
+            error: "something went wrong", 
+            message: error.message,
+        });
     }
 });
 
+router.post("/login", async(req, res) => {
+    try {
+        const existingUser = await User.findOne({
+            email: req.body.email 
+        });
+
+        if(!existingUser){
+            return res.status(404).json({
+                message: "user not found",
+            });
+        }
+
+        const isValid = await bcrypt.compare(
+            req.body.password,
+            existingUser.password,
+        );
+
+        if(!isValid){
+            return res.status(500).json({
+                message: "invalid credentials",
+            });
+        }
+
+        const token = jwt.sign(
+            {
+                _id: existingUser._id,
+                channelName: existingUser.channelName,
+                email: existingUser.email,
+                phone: existingUser.phone,
+                logoId: existingUser.logoId,
+            }, 
+            process.env.JWT_TOKEN, 
+            { expiresIn: "10d" }
+        );
+
+        res.status(200).json({
+            _id: existingUser._id,
+            channelName: existingUser.channelName,
+            email: existingUser.email,
+            phone: existingUser.phone,
+            logoId: existingUser.logoId,
+            logoUrl: existingUser.logoUrl,
+            token: token,
+            subscribers: existingUser.subscribers,
+            subscribedChannels: existingUser.subscribedChannels,
+        });
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            error: "something went wrong",
+            message: error.message,
+        });
+    }
+})
 
 export default router; 
