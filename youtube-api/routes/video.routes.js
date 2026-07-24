8e37a84ea436243c64a5f2e922e9e194084f04b3
Vansh Baranwal <vanshveerbaranwal@gmail.com>
2026-07-24 23:55:09 +0530

    implemented the update video route

--- youtube-api/routes/video.routes.js
+++ youtube-api/routes/video.routes.js
@@ -57,7 +57,56 @@ router.post("/upload", checkAuth, async(req, res) => {
 
 // update video (no video change only metadata and thumbnail change)
 
-router.post("/update")
+router.post("/update:id", checkAuth, async(req, res) => {
+    try {
+        const { title, description, category, tags } = req.body;
+        const videoId = req.params.id;
+
+        // find video by id
+        let video = await Video.findById(videoId);
+        if(!video){
+            return res.status(404).json({
+                error: "videos not found",
+            });
+        }
+
+        if(video.user_id.toString() !== req.user._id.toString()){
+            return res.status(403).json({
+                error: "unauthorized",
+            });
+        }
+
+        if(req.files && req.files.thumbnail){
+            await cloudinary.uploader.destroy(video.thumbnailId);
+
+            const thumbnailUpload = await cloudinary.uploader.upload(req.files.thumbnail.tempFilePath, {
+                folder: "thumbnail",
+            });
+
+            video.thumbnailUrl = thumbnailUpload.secure_url;
+            video.thumbnailId = thumbnailUpload.public_id;
+
+        }
+
+        video.title = title || video.title;
+        video.description = description || video.description;
+        video.category = category || video.category;
+        video.tags = tags || video.tags;
+
+        await video.save();
+        res.status(200).json({
+            message: "video updated succcessfully", 
+            video,
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            error: "something went wrong",
+            message: error.message,
+        });
+    }
+});
 
 
 export default router;
